ecf35205ac9f9390d6ca52385155b6d7ee333ca3
Julio Berina <julioberina@gmail.com>
2017-11-01 21:02:51 -0700

    Changed algorithm from Cramer's Rule to scaled partial pivoting

--- main.cpp
+++ main.cpp
@@ -17,122 +17,101 @@ it exits the program.
 #include <cmath>
 using namespace std;
 
-float determinant(const vector<vector<float>>& matrix);
-vector<vector<float>> augmentedMatrix(vector<vector<float>> matrix);
-vector<vector<float>> cramerMatrix(vector<vector<float>> matrix, int col); // swaps column of matrix with solution vector
-vector<vector<float>> slicedMatrix(vector<vector<float>> matrix, int row, int col); // excludes the row and col provided of the matrix 
-vector<float> solve(const vector<vector<float>>& matrix);
-
 int main()
 {
-    string fname;
-    string matrixLine; // Reading lines from iFile
-    string holder; // holds string of a matrix [row][col] value
-    vector<vector<float>> matrix;
-    vector<float> uniqueSolution;
+	string fname;
+	string matrixLine; // Reading lines from iFile
+	string holder; // holds string of a matrix [row][col] value
+	vector<vector<float>> matrix;
+	float* solution;
+	int n = 0; // matrix dimensions
+	int i, j, k, p;
+	float app, sum, mult;
 
-    cout << "Enter file name to read matrix from: ";
-    getline(cin, fname);
+	cout << "Enter file name to read matrix from: ";
+	getline(cin, fname);
 
-    ifstream iFile(fname, ifstream::in);
-    if (iFile.fail())
-        return 1;
+	ifstream iFile(fname, ifstream::in);
+	if (iFile.fail())
+		return 1;
 
-    // Determine the dimensions of the matrix
-    while (getline(iFile, matrixLine))
-    {
-        stringstream fss(matrixLine);
-        vector<float> rowVec;
+	// Determine the dimensions of the matrix
+	while (getline(iFile, matrixLine))
+	{
+		++n;
+		stringstream fss(matrixLine);
+		vector<float> rowVec;
 
-        while (!fss.eof())
-        {
-            fss >> holder;
-            rowVec.push_back(stof(holder));
-        }
+		while (!fss.eof())
+		{
+			fss >> holder;
+			rowVec.push_back(stof(holder));
+		}
 
-        matrix.push_back(rowVec);
-    }
+		matrix.push_back(rowVec);
+	}
 
-    iFile.close();
-    uniqueSolution = solve(matrix);
+	iFile.close();
+	solution = new float[n];
 
-    if (uniqueSolution.empty())
-    {
-        cerr << "No unique solution!  Exiting program..." << endl;
-        return 1;
-    }
+	// Initialize solution array to zero
+	for (int i = 0; i < n; ++i)
+		solution[i] = 0;
 
-    cout << "Unique solution:" << endl;
+	//Fill in the values of the matrix
+	for (i = 0; i<(n); i++) 
+	{
+		app = matrix[i][i];
 
-    for (int i = 0; i < uniqueSolution.size(); ++i)
-        cout << uniqueSolution[i] << " ";
+		//initialization of p
+		p = i;
 
-    cout << endl;
+		//find largest no of the columns and row no. of largest no.
+		for (k = i + 1; k < n; k++)
+		{
+			if (fabs(app) < fabs(matrix[k][i])) 
+			{
+				app = matrix[k][i];
+				p = k;
+			}
+		}
 
-    return 0;
-}
+		//swaping the elements of diagonal row and row containing largest no
+		for (j = 0; j <= n; j++)
+		{
+			solution[j] = matrix[p][j];
+			matrix[p][j] = matrix[i][j];
+			matrix[i][j] = solution[j];
+		}
 
-float determinant(const vector<vector<float>>& matrix)
-{
-    float det = 0.0;
+		//calculating triangular matrix
+		for (j = i + 1; j < n; j++) 
+		{
+			mult = matrix[j][i] / matrix[i][i];
 
-    if (matrix.size() == 2)
-        return ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]));
-    else
-    {
-        // Obtain determinant by using cofactors and recursion
-        for (int col = 0; col < matrix[0].size(); ++col)
-            det += (pow(-1, col) * matrix[0][col] * determinant(slicedMatrix(matrix, 0, col)));
-        
-        return det;
-    }
-}
+			for (k = 0; k <= n; k++)
+				matrix[j][k] -= (mult*matrix[i][k]);
+		}
+	}
 
-vector<vector<float>> augmentedMatrix(vector<vector<float>> matrix)
-{
-    for (int row = 0; row < matrix.size(); ++row)
-        matrix[row].pop_back();
+	//for calculating value of z,y,x via backward substitution method
+	for (i = n - 1; i >= 0; i--)
+	{
+		sum = 0;
+		for (j = i + 1; j < n; j++)
+			sum += (matrix[i][j] * solution[j]);
 
-    return matrix;
-}
+		solution[i] = (matrix[i][n] - sum) / matrix[i][i];
+	}
 
-vector<vector<float>> cramerMatrix(vector<vector<float>> matrix, int col)
-{
-    float temp = 0.0;
-    int solColumn = matrix[0].size() - 1;
+	//display solution
+	cout << endl << "Solution:" << endl;
+	for (i = 0; i < n; i++)
+		cout << "X[" << i + 1 << "] = " << solution[i] << endl;
 
-    for (int row = 0; row < matrix.size(); ++row)
-    {
-        temp = matrix[row][col];
-        matrix[row][col] = matrix[row][solColumn];
-        matrix[row][solColumn] = temp;
-        matrix[row].pop_back();
-    }
+	cout << endl;
 
-    return matrix;
-}
-
-vector<vector<float>> slicedMatrix(vector<vector<float>> matrix, int row, int col)
-{
-    matrix.erase(matrix.begin()+row);
-
-    for (int r = 0; r < matrix.size(); ++r)
-        matrix[r].erase(matrix[r].begin()+col);
-
-    return matrix;
-}
-
-vector<float> solve(const vector<vector<float>>& matrix)
-{
-    vector<float> unique;
-    float det = determinant(augmentedMatrix(matrix));
-    int colSize = matrix[0].size() - 1; // take any row of the matrix to get column size minus solution vector
-
-    if (det != 0)
-    {
-        for (int c = 0; c < colSize; ++c)
-            unique.push_back((determinant(cramerMatrix(matrix, c)) / det));
-    }
-
-    return unique; // Returns unique whether or not a unique solution exists
+    delete [] solution;
+	system("pause");
+	return 0;
 }
